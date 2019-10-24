506719f4f85f7aae5110f8c13d682c058b46ec3d
Ivan Spasov <frankeys89@gmail.com>
2019-10-24 15:13:31 +0300

    Move to gradle

--- orka-teamcity-plugin-server/src/test/java/com/macstadium/orka/OrkaCloudClientFactoryTest.java
+++ orka-teamcity-plugin-server/src/test/java/com/macstadium/orka/OrkaCloudClientFactoryTest.java
@@ -9,20 +9,21 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.testng.annotations.Test;
-
 import jetbrains.buildServer.clouds.CloudRegistrar;
 import jetbrains.buildServer.serverSide.AgentDescription;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 
+import org.testng.annotations.Test;
+
 @Test
 public class OrkaCloudClientFactoryTest {
     public void when_can_be_agent_of_type_with_correct_values_should_return_true() throws IOException {
-        OrkaCloudClientFactory factory = new OrkaCloudClientFactory(mock(CloudRegistrar.class),
-                mock(PluginDescriptor.class));
         Map<String, String> params = new HashMap<String, String>();
         params.put(OrkaConstants.INSTANCE_ID_PARAM_NAME, "instanceId");
         params.put(OrkaConstants.IMAGE_ID_PARAM_NAME, "imageId");
+
+        OrkaCloudClientFactory factory = new OrkaCloudClientFactory(mock(CloudRegistrar.class),
+                mock(PluginDescriptor.class));
 
         AgentDescription agentDescription = mock(AgentDescription.class);
         when(agentDescription.getConfigurationParameters()).thenReturn(params);
